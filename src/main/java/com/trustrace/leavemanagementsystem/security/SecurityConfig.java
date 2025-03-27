7ee0d0f613335d5078425c622ddaf0b4cc5c072a
Dayanithi_E_K <dayanithiek.21cse@kongu.edu>
2025-03-27 11:37:27 +0530

    deployment

--- src/main/java/com/trustrace/leavemanagementsystem/security/SecurityConfig.java
+++ src/main/java/com/trustrace/leavemanagementsystem/security/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
         http
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowedOrigins(List.of("http://localhost:4200"));
+                    config.setAllowedOrigins(List.of("http://localhost:4200", "https://dayanithi24.github.io"));
                     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
                     return config;
